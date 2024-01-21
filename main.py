5ec564fade8c3aba232f5aa3e19c640e621c3293
MathisJANKOVIC <mathis.jankovic@gmail.com>
2024-01-22 00:13:38 +0100

    Rethink all project achitecture to object oriented

--- main.py
+++ main.py
@@ -1,44 +1,29 @@
-from list_element import ListElement
-import create_list
+from list_controller import ListManager
 import manage_list
-
 import pythonclimenu
-import json
-import sys
-import os
 
 SAVE_PATH = "save.json"
-MAIN_MENU = ("Create a new list", "Select an existing list", "Save and quit")
 
-lists: list[ListElement] = []
-
-if(os.path.exists(SAVE_PATH) and os.path.getsize(SAVE_PATH) > 0):
-    with open(SAVE_PATH, "r", encoding="utf-8") as save_file:
-        saved_lists: list = json.load(save_file)
-
-        for saved_list in saved_lists:
-            list_element = ListElement(saved_list["name"], saved_list["content"])
-            lists.append(list_element)
+list_manager = ListManager()
+list_manager.load_data_from(SAVE_PATH)
 
 choice = 0
 while(True):
-    choice = pythonclimenu.menu("Welcome to List Manager", MAIN_MENU, "blue", initial_cursor_position=choice)
+    choice = pythonclimenu.menu(
+        title = "Welcome to List Manager",
+        options = ("Create a new list", "Select an existing list", "Save and quit"),
+        cursor_color = "blue",
+        initial_cursor_position = choice
+    )
 
     if(choice == 0):
-        create_list.main(lists)
+        list_manager.create_list()
     elif(choice == 1):
-        if(len(lists) > 0):
-            manage_list.main(lists)
+        if(len(list_manager.lists) > 0):
+            manage_list.main(list_manager)
         else:
             print("\n\033[95m You haven't created any list yet \033[0m\n")
             input(" Press enter to continue...")
     else:
-        lists_to_save: list = []
-
-        for list_element in lists:
-            lists_to_save.append({"name" : list_element.name, "content" : list_element.content})
-
-        with open(SAVE_PATH, "w", encoding="utf-8") as save_file:
-            json.dump(lists_to_save, save_file, indent=4)
-
-        sys.exit()
+        list_manager.save(SAVE_PATH)
+        exit()
--- manage_list.py
+++ manage_list.py
@@ -1,15 +1,15 @@
+from list_controller import ListManager
 import pythonclimenu
-import sys
 
 OPERATION_OPTIONS = ("Add a new element", "Remove an element", "Empty the list", "Delete the list", "Return")
 CONFIRM_OPTIONS = ("Ok", "Cancel")
 LIST_CONTENT_MIN_MARGIN = 11
 
-def main(lists):
+def main(list_manager: ListManager):
     initial_cursor_position = 0
 
     while(True):
-        select_options = [list_element.name for list_element in lists] + ["Return"]
+        select_options = [list_element.name for list_element in list_manager.lists] + ["Return"]
         select_options_color = ["light_red" if option == "Return" else "white" for option in select_options]
 
         selected_option_index = pythonclimenu.menu(
@@ -21,7 +21,7 @@ def main(lists):
         )
 
         if(selected_option_index != select_options.index("Return")):
-            for list_element in lists:
+            for list_element in list_manager.lists:
                 if(list_element.name == select_options[selected_option_index]):
                     selected_list = list_element
                     break
@@ -59,82 +59,19 @@ def main(lists):
                 )
 
                 if(operation_to_perform_index == 0): # add new element
-                    sys.stdout.write("\n")
-
-                    while(True):
-                        new_element = input(f" Enter the element to add to '{selected_list.name}' : ")
-                        new_element = new_element.strip()
-
-                        sys.stdout.write("\n")
-                        sys.stdout.write("\033[K") # deletes potencily previous message error
-
-                        if(len(new_element) < 3):
-                            sys.stdout.write("\033[31m Elements cannot be shorter than 3 characters \033[0m")
-
-                        elif(len(new_element) > 20):
-                            sys.stdout.write("\033[31m Elements cannot be longer than 20 characters \033[0m")
-
-                        elif(not new_element.replace(" ","").replace("-","").isalnum()):
-                            sys.stdout.write("\033[31m Elements cannot contain special characters \033[0m")
-
-                        elif(new_element in [element for element in selected_list.content]):
-                            sys.stdout.write(f"\033[31m Element '{new_element}' already exists in '{selected_list.name}'\033[0m")
-                        else:
-                            break
-
-                        sys.stdout.write("\033[F" * 2)
-                        sys.stdout.write("\033[K")
-
-                    selected_list.add(new_element)
+                    list_manager.add_element_to(selected_list.name)
 
                 elif(operation_to_perform_index == 1): # remove an element
-                    if(len(selected_list.content) > 0):
-                        rm_options = selected_list.content + ["Cancel"]
+                    list_manager.remove_element_from(selected_list.name)
 
-                        sure_to_rm = None
-                        element_to_rm = 0
-
-                        # While the user doesn't cancel the operation or doesn't select an element to remove
-                        while(sure_to_rm != 0 and element_to_rm != rm_options.index("Cancel")):
-                            rm_options_color = ["white" if option == "Cancel" else "light_magenta" for option in rm_options]
-
-                            element_to_rm = pythonclimenu.menu(
-                                title = "Choose an element to remove",
-                                options = rm_options,
-                                cursor_color = "blue",
-                                options_color = rm_options_color,
-                                initial_cursor_position = element_to_rm
-                            )
-
-                            if(element_to_rm != rm_options.index("Cancel")):
-                                sure_to_rm = pythonclimenu.menu(
-                                    title = f"You are about to remove '{rm_options[element_to_rm]}' from '{selected_list.name}'",
-                                    options = CONFIRM_OPTIONS,
-                                    cursor_color = "red",
-                                    initial_cursor_position = -1
-                                )
-
-                                if(sure_to_rm == 0):
-                                    selected_list.remove(rm_options[element_to_rm])
-                    else:
-                        print("\n\033[95m You cannot remove an element, the list is empty \033[0m\n")
-                        input(" Press enter to continue...")
-
-                elif(operation_to_perform_index == 2): # empty the list
-                    if(len(selected_list.content) > 0):
-                        sure_to_clear = pythonclimenu.menu(f"You are about to clear list '{selected_list.name}'", CONFIRM_OPTIONS, "red", initial_cursor_position=-1)
-
-                        if(sure_to_clear == 0):
-                            selected_list.clear()
-                    else:
-                        print("\n\033[95m The list is already empty \033[0m\n")
-                        input(" Press enter to continue...")
+                elif(operation_to_perform_index == 2): # clear the list
+                    list_manager.clear(selected_list.name)
 
                 elif(operation_to_perform_index == 3): # delete the list
                     sure_to_delete = pythonclimenu.menu(f"You are about to delete list '{selected_list.name}'", CONFIRM_OPTIONS, "red", initial_cursor_position=-1)
 
                     if(sure_to_delete == 0):
-                        lists.remove(selected_list)
+                        list_manager.lists.remove(selected_list)
                         initial_cursor_position = 0
                         break
                 else:
