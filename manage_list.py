113f3322fcdcc032f41e9d7338ca783ef5a49cc7
MathisJANKOVIC <mathis.jankovic@gmail.com>
2024-01-21 01:52:59 +0100

    Improve optimization & lisibility

--- manage_list.py
+++ manage_list.py
@@ -2,55 +2,60 @@ import pythonclimenu
 import sys
 
 OPERATION_OPTIONS = ("Add a new element", "Remove an element", "Empty the list", "Delete the list", "Return")
-CONFIRMATION_OPTIONS = ("Ok", "Cancel")
+CONFIRM_OPTIONS = ("Ok", "Cancel")
 LIST_CONTENT_MIN_MARGIN = 11
 
-def main(lists, initial_cursor_position=0):
-    list_manager_options = [list_element.name for list_element in lists] + ["Return"]
-    options_color = ["white" if list_manager_options[i] != "Return" else "light_red" for i in range(len(list_manager_options))]
+def main(lists, initial_cursor_position = 0):
+    select_options = [list_element.name for list_element in lists] + ["Return"]
+    select_options_color = ["light_red" if option == "Return" else "white" for option in select_options]
 
-    selected_list_index = pythonclimenu.menu("Select a list to manage", list_manager_options, "blue", options_color=options_color, initial_cursor_position=initial_cursor_position)
+    selected_option_index = pythonclimenu.menu(
+        title = "Select a list to manage",
+        cursor_color = "blue",
+        options = select_options,
+        options_color = select_options_color,
+        initial_cursor_position = initial_cursor_position
+    )
 
-    if(selected_list_index != "Return"):
-        for list in lists:
-            if(list.name == list_manager_options[selected_list_index]):
-                selected_list = list
+    if(selected_option_index != select_options.index("Return")):
+        for list_element in lists:
+            if(list_element.name == select_options[selected_option_index]):
+                selected_list = list_element
                 break
 
-        action = 0
-        while(action != "Return"):
-            list_content = [f"- {element}" for element in list.content]
+        operation_to_perform_index = 0
+        while(operation_to_perform_index != OPERATION_OPTIONS.index("Return")):
+            list_content = [f"- {element}" for element in list_element.content]
 
             if(len(list_content) > 0):
                 longer_element = max(list_content, key=len)
 
-                if(len(longer_element) < LIST_CONTENT_MIN_MARGIN): # a changer
-                    for i, element in enumerate(list_content):
-                        list_content[i] = element.ljust(LIST_CONTENT_MIN_MARGIN)
-                else:
+                # Adjust the margin to the longest element else use a default margin
+                if(len(longer_element) > LIST_CONTENT_MIN_MARGIN):
                     for i, element in enumerate(list_content):
                         list_content[i] = element.ljust(len(longer_element))
+                else:
+                    for i, element in enumerate(list_content):
+                        list_content[i] = element.ljust(LIST_CONTENT_MIN_MARGIN)
 
-                title_menu = [f"{list.name} content :\n"] + list_content
+                title_operation_menu = [f"{list_element.name} content :\n"] + list_content
+                title_operation_color = ["light_magenta" if line.startswith("-") else "white" for line in title_operation_menu]
             else:
-                title_menu = selected_list.name + " (empty)"
+                title_operation_menu = selected_list.name + " (empty)"
+                title_operation_color = "white"
 
-            if(type(title_menu) is str):
-                title_color = None
-            else:
-                title_color = ["light_magenta" if title_menu[i].startswith("-") else None for i in range(len(title_menu))]
+            operation_options_color = ["light_red" if option == "Return" else "light_yellow" for option in OPERATION_OPTIONS]
 
-            options_color = ["light_yellow" if OPERATION_OPTIONS[i] != "Return" else "light_red" for i in range(len(OPERATION_OPTIONS))]
-            action = pythonclimenu.menu(
-                title = title_menu,
+            operation_to_perform_index = pythonclimenu.menu(
+                title = title_operation_menu,
                 options = OPERATION_OPTIONS,
                 cursor_color = "blue",
-                title_color = title_color,
-                options_color = options_color,
-                initial_cursor_position = action
+                title_color = title_operation_color,
+                options_color = operation_options_color,
+                initial_cursor_position = operation_to_perform_index
             )
 
-            if(action == 0): # add new element
+            if(operation_to_perform_index == 0): # add new element
                 sys.stdout.write("\n")
 
                 while(True):
@@ -79,18 +84,30 @@ def main(lists, initial_cursor_position=0):
 
                 selected_list.add(new_element)
 
-            elif(action == 1): # remove an element
+            elif(operation_to_perform_index == 1): # remove an element
                 if(len(selected_list.content) > 0):
                     rm_options = selected_list.content + ["Cancel"]
 
                     sure_to_rm = None
                     element_to_rm = 0
-                    while(sure_to_rm != 0 and element_to_rm != -1):
-                        options_color = ["light_magenta" if rm_options[i] != "Cancel" else "white" for i in range(len(rm_options))]
-                        element_to_rm = pythonclimenu.menu("Choose an element to remove", rm_options, "blue", options_color=options_color, initial_cursor_position=element_to_rm)
+                    while(sure_to_rm != 0 and element_to_rm != -1): # while the user doesn't cancel the operation or doesn't select an element to remove
+                        rm_options_color = ["white" if option == "Cancel" else "light_magenta" for option in rm_options]
 
-                        if(element_to_rm != -1):
-                            sure_to_rm = pythonclimenu.menu(f"You are about to remove '{rm_options[element_to_rm]}' from '{selected_list.name}'", CONFIRMATION_OPTIONS, "red", initial_cursor_position=-1)
+                        element_to_rm = pythonclimenu.menu(
+                            title = "Choose an element to remove",
+                            options = rm_options,
+                            cursor_color = "blue",
+                            options_color = rm_options_color,
+                            initial_cursor_position = element_to_rm
+                        )
+
+                        if(element_to_rm != rm_options.index(CONFIRM_OPTIONS[1])):
+                            sure_to_rm = pythonclimenu.menu(
+                                title = f"You are about to remove '{rm_options[element_to_rm]}' from '{selected_list.name}'",
+                                options = CONFIRM_OPTIONS,
+                                cursor_color = "red",
+                                initial_cursor_position = -1
+                            )
 
                             if(sure_to_rm == 0):
                                 selected_list.remove(rm_options[element_to_rm])
@@ -98,9 +115,9 @@ def main(lists, initial_cursor_position=0):
                     print("\n\033[95m You cannot remove an element, the list is empty \033[0m\n")
                     input(" Press enter to continue...")
 
-            elif(action == 2): # empty the list
+            elif(operation_to_perform_index == 2): # empty the list
                 if(len(selected_list.content) > 0):
-                    sure_to_clear = pythonclimenu.menu(f"You are about to clear list '{selected_list.name}'", CONFIRMATION_OPTIONS, "red", initial_cursor_position=-1)
+                    sure_to_clear = pythonclimenu.menu(f"You are about to clear list '{selected_list.name}'", CONFIRM_OPTIONS, "red", initial_cursor_position=-1)
 
                     if(sure_to_clear == 0):
                         selected_list.clear()
@@ -108,13 +125,31 @@ def main(lists, initial_cursor_position=0):
                     print("\n\033[95m The list is already empty \033[0m\n")
                     input(" Press enter to continue...")
 
-            elif(action == 3): # delete the list
-                sure_to_clear = pythonclimenu.menu(f"You are about to delete list '{selected_list.name}'", CONFIRMATION_OPTIONS, "red", initial_cursor_position=-1)
+            elif(operation_to_perform_index == 3): # delete the list
+                sure_to_delete = pythonclimenu.menu(f"You are about to delete list '{selected_list.name}'", CONFIRM_OPTIONS, "red", initial_cursor_position=-1)
 
-                if(sure_to_clear == 0):
+                if(sure_to_delete == 0):
                     lists.remove(selected_list)
                     main(lists)
             else:
-                main(lists, selected_list_index)
+                main(lists, selected_option_index)
     else:
         return
+
+if(__name__ == "__main__"):
+    import os
+    from list_element import ListElement
+    import json
+    SAVE_PATH = "save.json"
+    MAIN_MENU = ("Create a new list", "Select an existing list", "Save and quit")
+
+    lists: list[ListElement] = []
+
+    if(os.path.exists(SAVE_PATH) and os.path.getsize(SAVE_PATH) > 0):
+        with open(SAVE_PATH, "r", encoding="utf-8") as save_file:
+            saved_lists: list = json.load(save_file)
+
+            for saved_list in saved_lists:
+                list_element = ListElement(saved_list["name"], saved_list["content"])
+                lists.append(list_element)
+    main(lists)
